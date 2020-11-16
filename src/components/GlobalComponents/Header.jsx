11827912cba1d589b9563910c0338c49e8a17100
PrimeWalkerVN <nchithanh609@gmail.com>
2020-11-16 16:52:18 +0700

    refactor code

--- src/components/GlobalComponents/Header.jsx
+++ src/components/GlobalComponents/Header.jsx
@@ -12,8 +12,10 @@ const Header = props => {
   );
 
   return (
-    <header onClick={redirectHomeHandler} className="flex justify-between items-center bg-blue-600 py-4 px-10 cursor-pointer">
-      <div className="text-white text-2xl font-serif">{name}</div>
+    <header className="flex justify-between items-center bg-blue-600 py-4 px-10">
+      <div onClick={redirectHomeHandler} className="text-white text-2xl font-serif cursor-pointer">
+        {name}
+      </div>
       <Dropdown className="cursor-pointer" overlay={menu} placement="bottomCenter">
         <div className="flex items-center">
           <span className="text-xl font-bold text-white mr-4">{username}</span>
