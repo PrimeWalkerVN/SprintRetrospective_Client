7385ac4d4592e138994435dda4fb56aee0b2b256
PrimeWalkerVN <nchithanh609@gmail.com>
2020-11-17 19:56:31 +0700

    update router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createBrowserHistory } from 'history';
 import { Router, Redirect, Route, Switch } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -13,21 +13,22 @@ import { setUser, setLogged } from './redux/reducers/userReducer';
 import PrivateRouteAuth from './components/Auth/privateRouteAuth';
 import Register from './components/Auth/Register';
 import queryString from 'query-string';
-import { setIsLoading } from './redux/reducers/loadingReducer';
+
 const history = createBrowserHistory();
 function App() {
   const isLoading = useSelector(state => state.loading.isLoading);
   const dispatch = useDispatch();
   const query = queryString.parse(history.location.search);
+  const [loading, setLoading] = useState(true);
   if (query.token) {
     localStorage.setItem('access_token', query.token);
   }
 
   const token = localStorage.getItem('access_token');
   useEffect(() => {
+    setLoading(true);
     if (token) {
       const checkLogged = async () => {
-        dispatch(setIsLoading(true));
         try {
           const user = await usersApi.getMe();
 
@@ -38,25 +39,30 @@ function App() {
         } catch (e) {
           dispatch(setLogged(false));
         }
-        dispatch(setIsLoading(false));
+        setLoading(false);
       };
       checkLogged();
+    } else {
+      setLoading(false);
     }
   }, [token, dispatch]);
   return (
     <div className="container max-w-full">
       {isLoading && <Loading />}
-      <Router history={history}>
-        <Switch>
-          <PrivateRouteAuth path="/login" component={Login} />
-          <PrivateRouteAuth path="/register" component={Register} />
-          <PrivateRoute path="/public-board/detail/:id" component={PublicBoardDetail} />
-          <PrivateRoute path="/dashboard" component={DashBoard} />
-
-          <Redirect exact from="/" to="/dashboard" />
-          <Route component={NotFound} />
-        </Switch>
-      </Router>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Router history={history}>
+          <Switch>
+            <PrivateRouteAuth path="/login" component={Login} />
+            <PrivateRouteAuth path="/register" component={Register} />
+            <PrivateRoute path="/public-board/detail/:id" component={PublicBoardDetail} />
+            <PrivateRoute path="/dashboard" component={DashBoard} />
+            <Redirect exact from="/" to="/dashboard" />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
+      )}
     </div>
   );
 }
